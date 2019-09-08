646ab47c948e56e980b863097f8ab7d643632ef7
yangchenglong2000 <1513409546@qq.com>
2019-09-08 13:55:51 +0800

    rename java.txt

---
+++ homework/java作业/AAA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AAA {
+public static void main(String[]args){
+	System.out.println("��Ƕ���С����" );
+    int leftHand=10;
+    int rightHand=8;
+    int a;
+    System.out.println("����"+leftHand);
+    System.out.println("����"+rightHand);
+    a=leftHand;
+    leftHand=rightHand;
+    rightHand=a;
+	System.out.println("����"+leftHand);
+	System.out.println("����"+rightHand);
+	Scanner input=new Scanner(System.in);
+}
+
+
+}
